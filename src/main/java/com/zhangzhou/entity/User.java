86a7b9b13d687cbe43c7b39cc8fde6053af4d8b6
zhou.zhang <zhou.zhang@weimob.com>
2018-12-25 14:22:33 +0800

    add entity

---
+++ src/main/java/com/zhangzhou/entity/User.java
@@ -0,0 +1,19 @@
+package com.zhangzhou.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String id;
+    private String userName;
+    private String password;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
